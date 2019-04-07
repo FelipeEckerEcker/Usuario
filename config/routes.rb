8f90bb9da4f02f20b4544315713b8449c3f7c581
AndersonNesi <nesi.anderson@outlook.com>
2019-04-07 19:06:40 -0300

    primeiro

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :professores
   resources :disciplinas
   resources :aulas
   resources :cursos
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_183224) do
+ActiveRecord::Schema.define(version: 2019_04_07_220156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2019_04_07_183224) do
     t.integer "notasTrabalho"
     t.integer "notasProvaLivro"
     t.string "atividades"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "professores", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.string "email"
+    t.string "endereco"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
