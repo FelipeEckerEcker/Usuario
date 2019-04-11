d99b35d016db3527669868ae5897ce7e3a71e111
Felipe Ecker <lipe_ecker@hotmail.com>
2019-04-11 00:28:43 -0300

    Usuario/

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  resources :professores
   resources :disciplinas
-  resources :aulas
->>>>>>> 8f90bb9da4f02f20b4544315713b8449c3f7c581
   resources :cursos
   resources :aulas
   resources :professores
